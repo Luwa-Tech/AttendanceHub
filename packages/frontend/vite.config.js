0dc2a6997e1e884baf97acb59ca3a5fdbb713fbd
Yusuf Umar Faruq <111466842+Luwa-Tech@users.noreply.github.com>
2025-01-08 08:50:22 +0000

    increased build chunk size

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            'react', 
+            'react-dom', 
+            'axios', 
+            'react-hook-form', 
+            'react-icons', 
+            'react-responsive', 
+            'react-router', 
+            'react-router-dom'
+          ]
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
